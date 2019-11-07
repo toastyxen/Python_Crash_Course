edf996dcf053541840299831909314a41c1298aa
Lloyd W Turk Jr <xerodill@gmail.com>
2019-11-07 14:55:34 -0500

    update

--- python_work/chapter01/hello_world.py
+++ python_work/chapter01/hello_world.py
@@ -1,8 +1,1 @@
-message = "Hello Python world!"
-print (message)
-
-message = "Hello Python Crash Course world!"
-print (message)
-
-message = "Hello Python Crash Course reader!"
-print (message)
+print ("Hello Python world!")
