db22cf65e15a2ba1a3422e105e0387dcd5297143
Lloyd W Turk Jr <31788118+toastyxen@users.noreply.github.com>
2020-08-21 21:56:33 -0400

    Update Settings.py

--- python_work/chapter13/settings.py
+++ python_work/chapter13/settings.py
@@ -32,3 +32,6 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
+
+        # Alien Settings
+        self.alien_speed = 1.0
